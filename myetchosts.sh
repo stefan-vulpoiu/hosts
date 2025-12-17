21e84780d371da55b3c4e8c53638a1b9a575989e
stefan-vulpoiu <vulpoiu_stefan@yahoo.com>
2025-12-17 12:45:51 +0200

    adaugare pe github

---
+++ myetchosts.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+
+cat /etc/hosts | while read ip nume rest
+do
+        if [[ "$ip" == \#* ]] || [[ -z "$ip" ]] || [[ "$nume" == "localhost" ]]
+        then
+                continue
+        fi
+
+ adresa_ip=$(nslookup "$nume" | grep "Address" | tail -n 1 | awk '{print $2}')
+
+        if [[ -n "$adresa_ip" ]] && [[ "ip" != "$adresa_ip" ]]; then
+                echo "Bogus IP for <$nume> in /etc/hosts!"
+        fi
+done
